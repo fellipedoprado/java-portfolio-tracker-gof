7bf91bb9a1d0913bd4f427a4e340a20901fc268a
fellipedoprado <fellipe.doprado@gmail.com>
2025-04-22 22:54:03 -0300

    initial commit

---
+++ src/test/java/com/dio/portfoliotracker/PortfoliotrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.portfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliotrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
